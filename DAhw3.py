34d13addb0ba986b7f2423cc1f8671b86daf2b79
QuyenAnh2002 <quyenanh72002@gmail.com>
2023-02-14 16:42:47 +0700

    first commit

--- DAhw3.py
+++ DAhw3.py
@@ -4,21 +4,72 @@ import numpy as np
 df = pd.read_csv("C:/Users/quyen/Downloads/10_OneNumSevCatSubgroupsSevObs.csv")
 
 df.head(10)
-
+df.describe()
 total_bill = df['total_bill']
 tip = df['tip']
 sex = df['sex']
+smoker = df['smoker']
 
-print(total_bill.sum())
-print(tip.sum())
+# Calculate	total	bill,	total	tips,	statistic	value	of	them
 
-df.describe()
+print("total:", total_bill.sum())
+print("tip:", tip.sum())
+
+
+#Number	of	man/women	book	the	table?	
+
 
 sex_value = sex.value_counts()
+sex_value
 print(sex_value['Male'] / sex_value['Female'])
+
+#Probability	that	a	women	will	book	a	table?	
 
 total_sex = sex_value['Male'] + sex_value['Female']
 
 sex_value_tb = (sex_value['Female'] / total_sex) * 100
 print(sex_value_tb )
+
+
+#Number	of	smoker/	non-smoker	
+ 
+smoker_value = smoker.value_counts()
+smoker_value
+print(smoker_value['Yes'] / smoker_value['No'])
+
+#Probability	that	a	smoker	will	book	a	table?
+total_smoker = smoker_value['Yes'] + smoker_value['No']
+total_smoker
+
+smoker_value_tb = (smoker_value['Yes'] / total_smoker) * 100
+print(smoker_value_tb)
+
+
+#Probability	that	a	smoker	is	a	women	will	book	a	table?
+
+F_smoker = df[df["sex"] == 'Female'][df["smoker"] == 'Yes']
+F_smoker
+
+print((len(F_smoker)/total_sex) * 100)
+
+
+#•Classify	by	time
+group_by_time = df.groupby(["time"]).value_counts()
+group_by_time
+
+#Classify	by	size
+group_by_size = df.groupby(["size"]).value_counts()
+group_by_size
+
+
+
+
+
+
+
+
+
+
+
+
 
